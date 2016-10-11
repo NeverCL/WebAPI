d95a1bbbe86a8fa4827d3de28efad08a25da0e0a
Never、C <caolin>
2016-10-11 11:10:31 +0800

    add HttpMessageHandler

---
+++ WebAPI/消息管道/DemoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace 消息管道
+{
+    public class DemoController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            var cfg = new HttpConfiguration();
+            cfg.MessageHandlers.Add(new Handler1());
+            cfg.MessageHandlers.Add(new Handler1());
+            cfg.MessageHandlers.Add(new Handler1());
+            var server = new MyServer(cfg);
+            server.Initialize();
+            return GetHandlers(server);
+        }
+
+        private IEnumerable<string> GetHandlers(MyServer server)
+        {
+            DelegatingHandler next = server;
+            yield return next.ToString();
+            while (next.InnerHandler != null)
+            {
+                yield return next.InnerHandler.ToString();
+                next = next.InnerHandler as DelegatingHandler;
+                if (next == null)
+                    break;
+            }
+        }
+    }
+    class MyServer : HttpServer
+    {
+        public MyServer(HttpConfiguration cfg) : base(cfg) { }
+
+        public new void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+    class Handler1 : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebAPI/消息管道/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace 消息管道
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var server = new HttpSelfHostServer(new HttpSelfHostConfiguration("http://localhost:10000")))
+            {
+                server.Configuration.Routes.MapHttpRoute("default", "{controller}");
+                server.OpenAsync();
+                Console.Read();
+            }
+
+            //HttpMessageHandler handler = new HttpClientHandler();
+
+            //DelegatingHandler delegatingHandler = new HttpServer();
+
+            //HttpServer server = new HttpServer();
+        }
+    }
+}
