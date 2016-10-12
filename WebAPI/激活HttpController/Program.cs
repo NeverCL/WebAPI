92af3821fb8ebe791a7089925a8570b1aac10f19
Never、C <caolin>
2016-10-12 14:57:36 +0800

    add 激活HttpController

---
+++ WebAPI/激活HttpController/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+
+namespace 激活HttpController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. HttpControllerDispatcher
+            HttpControllerDispatcher dispatcher = new HttpControllerDispatcher(new HttpConfiguration());
+
+            //2. HttpControllerDescriptor
+            HttpControllerDescriptor descriptor = new HttpControllerDescriptor(new HttpConfiguration(), "Demo", typeof(DemoController));
+            descriptor.CreateController(new HttpRequestMessage(HttpMethod.Get, "http://localhost/demo"));
+        }
+    }
+}
