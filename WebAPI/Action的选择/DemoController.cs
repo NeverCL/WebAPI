77a16e59676f1f254199612ecd98dbfd9f76246b
Never、C <caolin>
2016-10-13 14:49:36 +0800

    add Action的选择

---
+++ WebAPI/Action的选择/DemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Action的选择
+{
+    public class DemoController : ApiController
+    {
+        public string Get(int x)
+        {
+            return x + Guid.NewGuid().ToString();
+        }
+
+        public string Get(int x, int y)
+        {
+            return x + Guid.NewGuid().ToString();
+        }
+
+        public string Get(string x, string y)
+        {
+            return x + y;
+        }
+
+        public string Get(string x, string y, string z)
+        {
+            return x + y + z;
+        }
+    }
+}
---
+++ WebAPI/Action的选择/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+
+namespace Action的选择
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpActionDescriptor actionDescriptor = new ReflectedHttpActionDescriptor(new HttpControllerDescriptor(), null);
+            HttpParameterDescriptor parameterDescriptor = new ReflectedHttpParameterDescriptor(actionDescriptor, null);
+            IHttpActionSelector selector = new ApiControllerActionSelector();
+        }
+    }
+}
