1c24fdf817573a3849f84aad6385092d691fc6a4
Never、C <caolin>
2016-10-15 21:27:43 +0800

    fix show subRoutes

--- WebAPI/特性路由/Program.cs
+++ WebAPI/特性路由/Program.cs
@@ -22,8 +22,6 @@ namespace 特性路由
 
             //HttpConfigurationExtensions.MapHttpAttributeRoutes(new HttpConfiguration());
             EnableRouteAttr();
-
-
         }
 
         private static void EnableRouteAttr()
@@ -33,9 +31,58 @@ namespace 特性路由
                 server.Configuration.MapHttpAttributeRoutes();
                 server.Configuration.Routes.MapHttpRoute("default", "{controller}");
                 server.Configuration.EnsureInitialized();
+                ShowAttrRoute(server.Configuration.Routes);
+                ShowSubRoutesToken(server.Configuration.Routes);
+                ShowRoutes(server.Configuration.Routes);
                 server.OpenAsync();
                 Console.Read();
             }
+        }
+
+        private static void ShowSubRoutesToken(HttpRouteCollection routes)
+        {
+            foreach (var subRoute in routes.GetSubRoutes())
+            {
+                Console.WriteLine(subRoute.RouteTemplate);
+                foreach (var dataToken in subRoute.DataTokens)
+                {
+                    Console.WriteLine("{0,-12}{1}", dataToken.Key, dataToken.Value);
+                }
+                Console.WriteLine();
+            }
+        
+        }
+
+        private static void ShowAttrRoute(HttpRouteCollection routes)
+        {
+            var subRoutes = routes.GetSubRoutes();
+            if (subRoutes == null)
+            {
+                Console.WriteLine("未注册特性路由");
+                return;
+            }
+            ShowRoutes(subRoutes);
+        }
+
+        private static void ShowRoutes(IEnumerable<IHttpRoute> routes)
+        {
+            Console.WriteLine("{0,-10}{1}", "类型名称", "模板");
+            foreach (var route in routes)
+            {
+                Console.WriteLine("{0,-10}{1}", route.GetType().Name, route.RouteTemplate);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public static class RouteCollectionExt
+    {
+        public static IEnumerable<IHttpRoute> GetSubRoutes(this HttpRouteCollection routes)
+        {
+            var route = routes["MS_attributerouteWebApi"];
+            var prop = route.GetType().GetProperty("SubRoutes", BindingFlags.Instance | BindingFlags.NonPublic);
+            var subRoutes = prop.GetValue(route) as IEnumerable<IHttpRoute>;
+            return subRoutes;
         }
     }
 }
