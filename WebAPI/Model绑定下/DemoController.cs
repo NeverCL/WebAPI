538a8d1a1512acbc359cbe1c9a0ff3457f25ccee
Never、C <caolin>
2016-10-28 00:48:14 +0800

    add modelbinder

---
+++ WebAPI/Model绑定下/DemoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.ModelBinding.Binders;
+
+namespace Model绑定下
+{
+    public class DemoController : ApiController
+    {
+        [Route("demo/get")]
+        public string Get()
+        {
+            return "Hello World";
+        }
+
+        [Route("get/{x}/{y}/{z}")]
+        public IEnumerable<Model> Get(Model model, [ModelBinder(Name = "m")] Model model2)
+        {
+            yield return model;
+            yield return model2;
+        }
+
+
+        [Route("get")]
+        public IEnumerable<int> Get([ModelBinder]IEnumerable<int> strs)
+        {
+            return strs;
+        }
+
+        [Route("get")]
+        public int[] Get(int[] strs)
+        {
+            return strs;
+        }
+    }
+
+    [ModelBinder]
+    public class Model
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+        public string Z { get; set; }
+    }
+}
---
+++ WebAPI/Model绑定下/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.ModelBinding.Binders;
+using System.Web.Http.SelfHost;
+using Newtonsoft.Json.Serialization;
+using IValueProvider = System.Web.Http.ValueProviders.IValueProvider;
+
+namespace Model绑定下
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var server = new HttpSelfHostServer(new HttpSelfHostConfiguration("http://localhost:10000")))
+            {
+                server.Configuration.MapHttpAttributeRoutes();
+                server.OpenAsync();
+                Console.ReadKey();
+            }
+
+            IModelBinder modelBinder;
+
+            CompositeModelBinder compositeModelBinder;
+
+            ModelBinderProvider modelBinderProvider;
+
+            CompositeModelBinderProvider compositeModelBinderProvider;
+
+            ModelBinderAttribute modelBinderAttribute;
+
+            TypeConverterModelBinder typeConverterModelBinder;
+
+            TypeConverterModelBinderProvider typeConverterModelBinderProvider;
+
+
+            ComplexModelDto complexModelDto;
+
+            MutableObjectModelBinder mutableObjectModelBinder;
+
+            ComplexModelDtoModelBinder complexModelDtoModelBinder;
+
+
+            CollectionModelBinder<int> collectionModelBinder;
+
+            ArrayModelBinder<int> arrayModelBinder;
+
+            DictionaryModelBinder<int, int> dictionaryModelBinder;
+        }
+    }
+}
