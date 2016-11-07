213b2cd63ad54c0349bf02157f71eb4bad612f1d
Never、C <caolin>
2016-11-07 16:46:37 +0800

    add parameterBinding

---
+++ WebAPI/参数绑定/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http.Controllers;
+using System.Web.Http.ModelBinding;
+
+namespace 参数绑定
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHttpActionSelector actionSelector;
+            HttpActionContext actionContext;
+            HttpParameterBinding httpParameterBinding;
+            IActionValueBinder actionValueBinder;
+            DefaultActionValueBinder defaultActionValueBinder;
+            ModelBinderParameterBinding modelBinderParameterBinding;
+        }
+    }
+}
